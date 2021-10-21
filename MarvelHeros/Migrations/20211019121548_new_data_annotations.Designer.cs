876608ddc5387539c283df19a63174c3c4fd8655
Alex Millerman <amillerman@users.noreply.github.com>
2021-10-21 06:32:33 -0400

    Completed validation tutorial

--- MarvelHeros/Migrations/20211019121548_new_data_annotations.Designer.cs
+++ MarvelHeros/Migrations/20211019121548_new_data_annotations.Designer.cs
@@ -2,14 +2,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MarvelHeros.Migrations
 {
     [DbContext(typeof(MarvelHerosHeroContext))]
-    partial class MarvelHerosHeroContextModelSnapshot : ModelSnapshot
+    [Migration("20211019121548_new_data_annotations")]
+    partial class new_data_annotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MarvelHeros/Models/Hero.cs
+++ MarvelHeros/Models/Hero.cs
@@ -14,6 +14,7 @@ namespace MarvelHeros.Models
 
         [Display(Name = "Date got their powers")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime PowerAcquisitionDate { get; set; }
 
         [Display(Name = "Secret Identity")]
